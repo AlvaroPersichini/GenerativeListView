669c02ebd8f85d5e0bf132618ed1b051523c6ddc
AlvaroPersichini <alvaro.persichini@gmail.com>
2025-12-21 22:27:11 -0300

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+
+
+
+
+        ' ***************************************** ListView2  ***********************
+        Dim myExcel As Microsoft.Office.Interop.Excel.Application = CType(GetObject(, "Excel.Application"), Microsoft.Office.Interop.Excel.Application) ' Enlaza EXCEL y comprobar estado. (pag.177)
+        Dim oWorkbook As Microsoft.Office.Interop.Excel.Workbook = myExcel.ActiveWorkbook
+        Dim oWorkSheet As Microsoft.Office.Interop.Excel.Worksheet = oWorkbook.Worksheets.Item(1)
+
+        Dim oCATIA As New CatiaSession   ' Instancia un objeto de clase CatiaSession para enlazar la aplicacion y comprobar el estado actual de la misma. (pag.177)
+        ' esta parte hay que seguirlaporque supone que CATIA esta abierta y con un documento de producto activo. hay que hacer un manejador de esto.
+        Dim oProductDocument As ProductStructureTypeLib.ProductDocument = oCATIA.Application.ActiveDocument
+        Dim oPorduct As ProductStructureTypeLib.Product = oProductDocument.Product
+
+        oCATIA.Application.DisplayFileAlerts = False
+        myExcel.ScreenUpdating = False
+
+        ' oCATIA.AppCATIA.Interactive = False ' Esto hay que verlo porque CATIA queda sin menues luego de correr este programa con esta linea.
+
+        To_Excel.CompletaListView2(oPorduct, oWorkSheet, "C:\Temp")
+        Procedimientos.FormatoListView2(oWorkSheet)
+
+        oCATIA.AppCATIA.Interactive = True
+        myExcel.ScreenUpdating = True
+
+
+
+
+
+
+    End Sub
+
+End Module
